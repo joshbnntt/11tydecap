5648da0c2547eea01885d2978ebdd1353070d9c2
joshuabnntt <joshua.bennett@smartcop.com>
2023-04-17 13:16:23 -0500

    Playing with webc

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,15 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_includes/components/**/*.webc",
+  });
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
